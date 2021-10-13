a17ced851ca9209a3ae88be0a3ff48ce8ef38fbb
Crowdbotics <team@crowdbotics.com>
2021-10-13 19:36:07 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "lively_meadow_31389",
+  url: "https://lively_meadow_31389.botics.co",
+  api: "https://lively_meadow_31389.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
